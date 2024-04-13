d9b077a5630663083d25bdf732cf0b28c100ce12
Brunoacofra <dolareeu@gmail.com>
2024-04-13 19:09:43 -0300

    Fazendo vetores e Arraylist

---
+++ Src/ArrayeVetor.java
@@ -0,0 +1,18 @@
+package Src;
+
+import java.util.ArrayList;
+
+public class ArrayeVetor {
+    public static void main (String[] args){
+        //vetor tem um tamanho pre-determinado no inico da declaração e não se altera
+
+        int[] vetor = new int[4];
+        short[] vetor1 = new short[4];
+        String[] vetor2 = new String[2];
+        //ArrayList é dinamico porem so utiliza as classe das variaveis primitivas
+
+        ArrayList<Integer> array = new ArrayList<Integer>();
+        ArrayList<Short> array1 = new ArrayList<Short>();
+        ArrayList<String> array2 = new ArrayList<String>();
+    }
+}
